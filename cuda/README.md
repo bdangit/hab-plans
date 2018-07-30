126d05f011391d0831d34afc3131825df58443f6
Ben Dang <me@bdang.it>
2018-07-30 08:22:18 -0700

    update cuda plan

--- cuda/README.md
+++ cuda/README.md
@@ -1,1 +1,41 @@
-# Cuda
+# cuda
+
+[GPU-accelerated Libraries](https://developer.nvidia.com/cuda-zone) for Computing on NVIDIA devices.
+
+## Maintainers
+
+* The Habitat Maintainers: <humans@habitat.sh>
+* Ben Dang: <me@bdang.it>
+
+## Type of Package
+
+Binary package
+
+## Usage
+
+To compile cuda code (ie. `*.cu`), just add `core/cuda` to your `plan.sh`.  You will then have
+access to compiler tools like `nvcc`, `cuda-gdb` and more.  Furthermore, `LD_RUN_PATH`, `CFLAGS`,
+`CXXFLAGS`, `CPPFLAGS`, and `LDFLAGS` will be updated with any of the cuda shared/static libraries.
+
+If you require shared libraries during runtime, it is recommended that you add `core/cuda-libs` to
+`pkg_deps` and add `core/cuda` to `pkg_build_deps`.
+
+### Example `plan.sh`
+
+```shell
+pkg_name=myawesomecudaapp
+pkg_origin=myorigin
+
+pkg_deps=(
+  core/cuda-libs
+  core/gcc-libs
+)
+pkg_build_deps=(
+  core/cuda
+  core/make
+)
+
+do_build() {
+  nvcc -o myawesomecudaapp source.cu
+}
+```
--- cuda/plan.sh
+++ cuda/plan.sh
@@ -1,37 +1,41 @@
 pkg_name=cuda
 pkg_origin=core
-pkg_description="Nvidia CUDA Libraries"
-pkg_version=9.2.88
-_driverver=396.26
+pkg_description="GPU-accelerated Libraries for Computing on NVIDIA devices"
+pkg_version=9.2.148
+_driverver=396.37
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('custom')
-pkg_source="https://developer.nvidia.com/compute/${pkg_name}/9.2/Prod/local_installers/${pkg_name}_${pkg_version}_${_driverver}_linux"
+pkg_source="https://developer.nvidia.com/compute/${pkg_name}/9.2/Prod2/local_installers/${pkg_name}_${pkg_version}_${_driverver}_linux"
 pkg_filename="${pkg_name}_${pkg_version}_${_driverver}_linux.run"
-pkg_shasum=8d02cc2a82f35b456d447df463148ac4cc823891be8820948109ad6186f2667c
+pkg_shasum=f5454ec2cfdf6e02979ed2b1ebc18480d5dded2ef2279e9ce68a505056da8611
 pkg_upstream_url="https://developer.nvidia.com/cuda-zone"
 
+## NOTE: Much of this plan copies what Archlinux did to repackage cuda.
+##       ref: https://git.archlinux.org/svntogit/community.git/tree/trunk/PKGBUILD?h=packages/cuda
+
 pkg_deps=(
-  core/coreutils
-  core/gcc
   core/gcc-libs
   core/glibc
   core/ncurses
   core/python2
   core/jre8
+  core/coreutils
   core/busybox-static
 )
 pkg_build_deps=(
-  core/linux-headers
+  core/gcc
+  core/make
   core/patchelf
   core/perl
 )
 
-pkg_bin_dirs=(opt/cuda/bin)
+pkg_bin_dirs=(bin)
 pkg_lib_dirs=(
-  opt/cuda/lib64
-  opt/cuda/nvvm/lib64
+  lib64
+  lib64/stubs
+  nvvm/lib64
 )
-pkg_include_dirs=(opt/cuda/include)
+pkg_include_dirs=(include)
 
 do_before() {
   if [[ ! -r /usr/bin/perl ]]; then
@@ -45,43 +49,36 @@ do_before() {
   fi
 }
 
-do_download() {
-  do_default_download
-
-  # download patch
-  download_file "https://developer.nvidia.com/compute/cuda/9.2/Prod/patches/1/cuda_9.2.88.1_linux" cuda_9.2.88.1_linux d2f2d0e91959e4b9a93cd2fa82dced3541e3b8046c3ab7ae335d36f71dbbca13
-}
-
 do_unpack() {
-  pushd "${HAB_CACHE_SRC_PATH}" > /dev/null
+  pushd "${HAB_CACHE_SRC_PATH}" > /dev/null || return 1
   sh "${pkg_filename}" --extract="${HAB_CACHE_SRC_PATH}/${pkg_dirname}"
-  pushd "${pkg_dirname}" > /dev/null
+
+  pushd "${pkg_dirname}" > /dev/null || return 1
+
+  # shellcheck disable=SC2211
   ./cuda-*.run --noexec --keep
+
+  # shellcheck disable=SC2211
   ./cuda-samples*.run --noexec --keep
-  popd > /dev/null
-  popd > /dev/null
+
+  popd > /dev/null || return 1
+
+  popd > /dev/null || return 1
 }
 
 do_prepare() {
   # path hacks
   # 1rd sed line: sets right path to install man files
   # 2rd sed line: hack to lie installer, now detect launch script by root
-  # 3rd/4th/5th sed line: sets right path in .desktop files and other .desktop stuff (warnings by desktop-file-validate)
+  # 3rd sed line: sets right path in .desktop files and other .desktop stuff (warnings by desktop-file-validate)
   sed -e "s|/usr/share|${pkg_prefix}/share|g" \
       -e 's|can_add_for_all_users;|1;|g' \
-      -e 's|=\\"$prefix\\\"|=/opt/cuda|g' \
-      -e 's|Terminal=No|Terminal=false|g' \
-      -e 's|ParallelComputing|ParallelComputing;|g' \
+      -e 's|Terminal=No|Terminal=false|g' -e 's|ParallelComputing|ParallelComputing;|g' \
       -i pkg/install-linux.pl
-
-  # set right path in Samples Makefiles
-  sed 's|\$cudaprefix\\|\\/opt\\/cuda\\|g' -i pkg/install-sdk-linux.pl
 
   # use python2
   _fix_interpreter_in_path "pkg" '*.py' core/python2 bin/python
   _fix_interpreter_in_path "pkg" '*.py' core/coreutils bin/env
-
-  #TODO: fix sample findgllib_mk
 }
 
 do_build() {
@@ -89,120 +86,190 @@ do_build() {
 }
 
 do_install() {
-  cd pkg
+  pushd pkg > /dev/null || return 1
+
   export PERL5LIB=.
-  perl install-linux.pl -prefix="${pkg_prefix}/opt/cuda" --noprompt
-  perl install-sdk-linux.pl -cudaprefix="${pkg_prefix}/opt/cuda" -prefix="${pkg_prefix}/opt/cuda/samples" -noprompt
-  sh "${HAB_CACHE_SRC_PATH}/cuda_9.2.88.1_linux" --silent --accept-eula --installdir="${pkg_prefix}/opt/cuda"
+  perl install-linux.pl -prefix="${pkg_prefix}" -noprompt -nosymlink
+  perl install-sdk-linux.pl -cudaprefix="${pkg_prefix}" -prefix="${pkg_prefix}/samples" -noprompt
 
   # Hack we need because of glibc 2.26 (https://bugs.archlinux.org/task/55580)
   # without which we couldn't compile anything at all.
   # Super dirty hack. I really hope it doesn't break other stuff!
   # Hopefully we can remove this for later version of cuda.
-  sed -i "1 i#define _BITS_FLOATN_H" "${pkg_prefix}/opt/cuda/include/host_defines.h"
+  sed -i "1 i#define _BITS_FLOATN_H" "${pkg_prefix}/include/host_defines.h"
 
   # Needs gcc7
-  ln -s "$(pkg_path_for core/gcc)/bin/gcc" "${pkg_prefix}/opt/cuda/bin/gcc"
-  ln -s "$(pkg_path_for core/gcc)/bin/g++" "${pkg_prefix}/opt/cuda/bin/g++"
+  ln -s "$(pkg_path_for core/gcc)/bin/gcc" "${pkg_prefix}/bin/gcc"
+  ln -s "$(pkg_path_for core/gcc)/bin/g++" "${pkg_prefix}/bin/g++"
 
-  # # Install profile and ld.so.config files
+  # Install profile and ld.so.config files
   mkdir -p "${pkg_prefix}/etc/profile.d"
   cat <<EOF > "${pkg_prefix}/etc/profile.d/cuda.sh"
-export PATH=\$PATH:${pkg_prefix}/opt/cuda/bin
+export PATH=\$PATH:${pkg_prefix}/bin
 EOF
   chmod 0755 "${pkg_prefix}/etc/profile.d/cuda.sh"
 
   mkdir -p "${pkg_prefix}/etc/ld.so.conf.d"
   cat <<EOF > "${pkg_prefix}/etc/ld.so.conf.d/cuda.conf"
-${pkg_prefix}/opt/cuda/lib64
-${pkg_prefix}/opt/cuda/lib
-${pkg_prefix}/opt/cuda/nvvm/lib64
-${pkg_prefix}/opt/cuda/nvvm/lib
+${pkg_prefix}/lib64
+${pkg_prefix}/nvvm/lib64
 EOF
   chmod 0644 "${pkg_prefix}/etc/ld.so.conf.d/cuda.conf"
 
+  # Install EULA
   mkdir -p "${pkg_prefix}/share/licenses"
-  ln -s "${pkg_prefix}/opt/cuda/doc/pdf/EULA.pdf" "${pkg_prefix}/share/licenses/EULA.pdf"
+  ln -s "${pkg_prefix}/doc/pdf/EULA.pdf" "${pkg_prefix}/share/licenses/EULA.pdf"
 
   # Remove redundant man and samples
-  rm -fr "${pkg_prefix}/opt/cuda/doc/man"
-  rm -fr "${pkg_prefix}/opt/cuda/cuda-samples"
+  rm -fr "${pkg_prefix}/doc/man"
+  rm -fr "${pkg_prefix}/cuda-samples"
   rm -fr "${pkg_prefix}/usr/share/man/man3/deprecated.3"*
 
   # Remove included copy of java and link to system java
-  rm -fr  "${pkg_prefix}/opt/cuda/jre"
-  sed 's|../jre/bin/java|$(pkg_path_for core/jre8)/bin/java|g' \
-    -i "${pkg_prefix}/opt/cuda/libnsight/nsight.ini" \
-    -i "${pkg_prefix}/opt/cuda/libnvvp/nvvp.ini"
+  rm -fr "${pkg_prefix}/jre"
+  sed "s|../jre/bin/java|$(pkg_path_for core/jre8)/bin/java|g" \
+    -i "${pkg_prefix}/libnsight/nsight.ini" \
+    -i "${pkg_prefix}/libnvvp/nvvp.ini"
 
   # Remove unused files
-  rm -fr "${pkg_prefix}/opt/cuda/"{bin,samples}"/.uninstall_manifest_do_not_delete.txt"
-  rm -fr "${pkg_prefix}/opt/cuda/samples/uninstall_cuda_samples"*.pl
-  rm -fr "${pkg_prefix}/opt/cuda/bin/cuda-install-samples"*.sh
-  rm -fr "${pkg_prefix}/opt/cuda/bin/uninstall_cuda_toolkit"*.pl
+  rm -fr "${pkg_prefix}/"{bin,samples}"/.uninstall_manifest_do_not_delete.txt"
+  rm -fr "${pkg_prefix}/samples/uninstall_cuda_samples"*.pl
+  rm -fr "${pkg_prefix}/bin/cuda-install-samples"*.sh
+  rm -fr "${pkg_prefix}/bin/uninstall_cuda_toolkit"*.pl
 
-  # patch some bins
-  build_line "patch ${pkg_prefix}/opt/cuda/nvvm/bin/cicc"
-  patchelf --interpreter "$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2" \
-    --set-rpath "${LD_RUN_PATH}" \
-    "${pkg_prefix}/opt/cuda/nvvm/bin/cicc"
+  # Fix interpreters
+  fix_interpreter "${pkg_prefix}/bin/computeprof" core/busybox-static bin/sh
+  fix_interpreter "${pkg_prefix}/bin/nvvp" core/busybox-static bin/sh
+  fix_interpreter "${pkg_prefix}/bin/nsight" core/busybox-static bin/sh
+  fix_interpreter "${pkg_prefix}/bin/nsight_ee_plugins_manage.sh" core/busybox-static bin/sh
 
-  build_line "patch ${pkg_prefix}/opt/cuda/libnvvp/nvvp"
-  patchelf --interpreter "$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2" \
-    --set-rpath "${LD_RUN_PATH}" \
-    "${pkg_prefix}/opt/cuda/libnvvp/nvvp"
+  # Let the patching begin
+  CUDA_RUN_PATH="$(pkg_path_for gcc-libs)/lib:$(pkg_path_for glibc)/lib"
 
-  build_line "patch ${pkg_prefix}/opt/cuda/libnsight/nsight"
-  patchelf --interpreter "$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2" \
-    --set-rpath "${LD_RUN_PATH}" \
-    "${pkg_prefix}/opt/cuda/libnsight/nsight"
-
-  for bin in bandwidthTest busGrind deviceQuery nbody oceanFFT randomFog vectorAdd; do
-    build_line "patch ${pkg_prefix}/opt/cuda/bin/${bin}"
+  # Patch Bins
+  _cuda_bins=(
+    nvvm/bin/cicc
+    libnvvp/nvvp
+    libnsight/nsight
+    extras/demo_suite/bandwidthTest
+    extras/demo_suite/busGrind
+    extras/demo_suite/deviceQuery
+    extras/demo_suite/nbody
+    extras/demo_suite/oceanFFT
+    extras/demo_suite/randomFog
+    extras/demo_suite/vectorAdd
+    bin/bin2c
+    bin/cudafe++
+    bin/cuobjdump
+    bin/fatbinary
+    bin/nvcc
+    bin/nvdisasm
+    bin/nvlink
+    bin/nvprof
+    bin/nvprune
+    bin/ptxas
+    bin/cuda-gdbserver
+    bin/cuda-memcheck
+    bin/gpu-library-advisor
+  )
+  for bin in "${_cuda_bins[@]}"; do
+    build_line "patch ${pkg_prefix}/${bin}"
     patchelf --interpreter "$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2" \
-      --set-rpath "${LD_RUN_PATH}" \
-      "${pkg_prefix}/opt/cuda/extras/demo_suite/${bin}"
+      --set-rpath "${pkg_prefix}/lib64:${pkg_prefix}/nvvm/lib64:${CUDA_RUN_PATH}" \
+      "${pkg_prefix}/${bin}"
+    patchelf --shrink-rpath \
+      "${pkg_prefix}/${bin}"
   done
 
-  for bin in bin2c cudafe++ cuobjdump fatbinary nvcc nvdisasm nvlink nvprof nvprune ptxas; do
-    build_line "patch ${pkg_prefix}/opt/cuda/bin/${bin}"
-    patchelf --interpreter "$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2" \
-      --set-rpath "${LD_RUN_PATH}" \
-      "${pkg_prefix}/opt/cuda/bin/${bin}"
-  done
-
-  for bin in cuda-gdbserver cuda-memcheck gpu-library-advisor; do
-    build_line "patch ${pkg_prefix}/opt/cuda/bin/${bin}"
-    patchelf --interpreter "$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2" \
-      --set-rpath "${LD_RUN_PATH}" \
-      "${pkg_prefix}/opt/cuda/bin/${bin}"
-  done
-
-  # cuda-gdb
+  # Patch Cuda-gdb
   # note: libncurses 6.1 is "designed to be source-compatible with 5.0 through 6.0"
-  build_line "patch ${pkg_prefix}/opt/cuda/bin/cuda-gdb"
-  patchelf --interpreter "$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2" "${pkg_prefix}/opt/cuda/bin/cuda-gdb"
-  patchelf --replace-needed libncurses.so.5 libncurses.so.6 "${pkg_prefix}/opt/cuda/bin/cuda-gdb"
-  patchelf --set-rpath "${LD_RUN_PATH}" "${pkg_prefix}/opt/cuda/bin/cuda-gdb"
+  build_line "patch ${pkg_prefix}/bin/cuda-gdb"
+  patchelf --interpreter "$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2" "${pkg_prefix}/bin/cuda-gdb"
+  patchelf --replace-needed libncurses.so.5 libncurses.so.6 "${pkg_prefix}/bin/cuda-gdb"
+  patchelf --set-rpath "${CUDA_RUN_PATH}:$(pkg_path_for ncurses)/lib" "${pkg_prefix}/bin/cuda-gdb"
 
-  fix_interpreter "${pkg_prefix}/opt/cuda/bin/computeprof" core/busybox-static bin/sh
-  fix_interpreter "${pkg_prefix}/opt/cuda/bin/nvvp" core/busybox-static bin/sh
-  fix_interpreter "${pkg_prefix}/opt/cuda/bin/nsight" core/busybox-static bin/sh
-  fix_interpreter "${pkg_prefix}/opt/cuda/bin/nsight_ee_plugins_manage.sh" core/busybox-static bin/sh
-
-  # patch some libs
-  for lib in OpenCL accinj64 cublas cudart cufft cufftw cuinj64 curand cusolver cusparse nppc nppial nppicc nppicom nppidei nppif nppig nppim nppist nppisu nppim nppist nppisu nppitc npps nvToolsExt nvblas nvgraph nvrtc-builtins nvrtc; do
-    build_line "patch ${pkg_prefix}/opt/cuda/lib64/lib${lib}.so"
-    patchelf --set-rpath "${LD_RUN_PATH}" "${pkg_prefix}/opt/cuda/lib64/lib${lib}.so"
+  # Patch libraries
+  _cuda_libs=(
+    libOpenCL
+    libcufftw
+    libnppc
+    libnppif
+    libnppitc
+    libnvrtc-builtins
+    libaccinj64
+    libcuinj64
+    libnppial
+    libnppig
+    libnpps
+    libnvrtc
+    libcublas
+    libcurand
+    libnppicc
+    libnppim
+    libnvToolsExt
+    libcudart
+    libcusolver
+    libnppicom
+    libnppist
+    libnvblas
+    libcufft
+    libcusparse
+    libnppidei
+    libnppisu
+    libnvgraph
+  )
+  for lib in "${_cuda_libs[@]}"; do
+    build_line "patch ${pkg_prefix}/lib64/${lib}.so"
+    patchelf --set-rpath "${CUDA_RUN_PATH}" \
+      "${pkg_prefix}/lib64/${lib}.so"
   done
 
-  for lib in $(ls "${pkg_prefix}/opt/cuda/lib64/stubs/"*.so); do
-    build_line "patch ${lib}"
-    patchelf --set-rpath "${LD_RUN_PATH}" "${lib}"
+  _cuda_stubs_libs=(
+    libcublas
+    libcurand
+    libnppial
+    libnppif
+    libnppisu
+    libnvidia-ml
+    libcuda
+    libcusolver
+    libnppicc
+    libnppig
+    libnppitc
+    libnvrtc
+    libcufft
+    libcusparse
+    libnppicom
+    libnppim
+    libnpps
+    libcufftw
+    libnppc
+    libnppidei
+    libnppist
+    libnvgraph
+  )
+  for lib in "${_cuda_stubs_libs[@]}"; do
+    build_line "patch ${pkg_prefix}/lib64/stubs/${lib}.so"
+    patchelf --set-rpath "${CUDA_RUN_PATH}" \
+      "${pkg_prefix}/lib64/stubs/${lib}.so"
   done
+
+  build_line "patch ${pkg_prefix}/nvvm/lib64/libnvvm.so"
+  patchelf --set-rpath "${CUDA_RUN_PATH}" \
+    "${pkg_prefix}/nvvm/lib64/libnvvm.so"
+
+  build_line "patch ${pkg_prefix}/extras/CUPTI/lib64/libcupti.so"
+  patchelf --set-rpath "${CUDA_RUN_PATH}" \
+    "${pkg_prefix}/extras/CUPTI/lib64/libcupti.so"
+
+  popd > /dev/null || return 1
 }
 
 do_strip() {
+  return 0
+}
+
+do_check() {
   return 0
 }
 
