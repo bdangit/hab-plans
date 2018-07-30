c000031a4e15b6acff36547c0942c3bda9d20eac
Ben Dang <me@bdang.it>
2018-07-30 08:25:02 -0700

    add example plans

---
+++ cuda-cmake-example/plan.sh
@@ -0,0 +1,46 @@
+pkg_origin=bdangit
+pkg_name=cuda-cmake-example
+pkg_version='0.1.0'
+pkg_description="cuda cmake example"
+pkg_license=('MIT')
+pkg_maintainer='Ben Dang <me@bdang.it>'
+pkg_deps=(
+  core/gcc-libs
+  core/glibc
+  bdangit/cuda-libs
+)
+pkg_build_deps=(
+  bdangit/cuda
+  core/make
+  core/cmake
+)
+pkg_bin_dirs=(bin)
+
+BUILDDIR='build'
+
+do_prepare() {
+  mkdir -pv "${BUILDDIR}"
+}
+
+do_build() {
+  pushd "${BUILDDIR}" > /dev/null
+
+  CUDA_PATH=$(pkg_path_for bdangit/cuda)
+  GLIBC_PATH=$(pkg_path_for glibc)
+
+  cmake -DCMAKE_INSTALL_PREFIX="${pkg_prefix}" \
+    -DCUDA_TOOLKIT_ROOT_DIR="${CUDA_PATH}" \
+    -DCUDA_rt_LIBRARY="${GLIBC_PATH}/lib/librt.so" \
+    ..
+  make
+
+  popd > /dev/null
+}
+
+do_install() {
+  pushd "${BUILDDIR}" > /dev/null
+
+  make install
+
+  popd > /dev/null
+}
---
+++ cuda-samples/plan.sh
@@ -0,0 +1,35 @@
+pkg_origin=bdangit
+pkg_name=cuda-samples
+pkg_version='9.2'
+pkg_description="$(cat << EOF
+  Samples for CUDA Developers which demonstrates features in CUDA Toolkit
+EOF
+)"
+pkg_source="https://github.com/NVIDIA/${pkg_name}/archive/v${pkg_version}.tar.gz"
+pkg_license=('custom')
+pkg_maintainer='Ben Dang <me@bdang.it>'
+pkg_upstream_url="https://github.com/NVIDIA/cuda-samples"
+pkg_shasum="4b172847196d5b2ab645625dd7953ddf5cffa04b2a3c45689d33ef92fc953d90"
+pkg_deps=(
+  bdangit/cuda-libs
+  core/gcc-libs
+)
+pkg_build_deps=(
+  bdangit/cuda
+  core/make
+)
+
+pkg_bin_dirs=(bin)
+
+do_build() {
+  export CUDA_PATH=$(pkg_path_for bdangit/cuda)
+  make
+}
+
+do_check() {
+  attach
+}
+
+do_install() {
+  cp -av bin/* "${pkg_prefix}/bin/"
+}
